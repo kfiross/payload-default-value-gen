201bbe103d908fed755b71d97ec683087b7b1264
Kfir Matityahu <kfir25812@gmail.com>
2025-03-26 02:02:28 +0200

    initial plugon version

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Plugin
+# Generating default-value function Plugin
+
+Generating default-value function (like uuid) plugin for Payload CMS.
+
+# Documentation
+
+Please refer to the [documentations](https://authsmith.com/docs/plugins/payload) for the installation and usages.
---
+++ src/genDefaultValueForIdPlugin.ts
@@ -0,0 +1,53 @@
+import type {CollectionSlug, Config} from 'payload'
+
+
+export type PluginOptions = {
+  /**
+   * List of collections to generate for them default value for the id field
+   */
+  collections?: Partial<Record<CollectionSlug, true>>
+  defaultFunc: () => any
+  disabled?: boolean
+}
+
+/**
+ * Generating default-value-function (like uuid) plugin function.
+ *
+ * @param {PluginOptions} pluginOptions
+ * @returns {Plugin}
+ */
+export const genDefaultValueForIdPlugin =
+  (pluginOptions: PluginOptions) =>
+    (incomingConfig: Config): Config => {
+      // create copy of incoming config
+      let config = {...incomingConfig}
+      if (!config.collections) {
+        config.collections = []
+      }
+
+      /**
+       * If the plugin is disabled, do nothing
+       * If your plugin heavily modifies the database schema, you may want to remove this property.
+       */
+      if (pluginOptions.disabled) {
+        return config
+      }
+
+      for (let collection of config.collections) {
+        if (config.collections.map(c => c.slug).includes(collection.slug)) {
+          collection.hooks = collection.hooks || {}
+
+          collection.hooks.beforeValidate = [
+            ...((collection.hooks.beforeValidate as any[]) || []),
+            ({data}) => {
+              if (data && !data.id) {
+                return {...data, id: pluginOptions.defaultFunc()}
+              }
+              return data
+            },
+          ]
+        }
+      }
+
+      return config
+    }
