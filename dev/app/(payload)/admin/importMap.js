8c9adf031edb20c7b2a4725da66ff1324aa02e66
Kfir Matityahu <kfir25812@gmail.com>
2025-11-08 18:20:12 +0200

    update: added exapmle project

---
+++ dev/app/(payload)/admin/importMap.js
@@ -0,0 +1,9 @@
+import { BeforeDashboardClient as BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343 } from 'payload-default-value-gen/client'
+import { BeforeDashboardServer as BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f } from 'payload-default-value-gen/rsc'
+
+export const importMap = {
+  'payload-default-value-gen/client#BeforeDashboardClient':
+    BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343,
+  'payload-default-value-gen/rsc#BeforeDashboardServer':
+    BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f,
+}
